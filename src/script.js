fabb46a2b978386d62577840b2117d4eb0b5eb85
Demid Popravko <demid1498@gmail.com>
2022-05-20 02:56:54 +0400

    Bracket counter!

--- src/script.js
+++ src/script.js
@@ -1,4 +1,19 @@
-console.log("Js online..")
-function hello () {
-    alert('Hello from Git!')
+function checkEven () {
+    let a = document.getElementById('text').value;
+    if (a == 0) {
+        document.getElementById("output").innerHTML = ("Type smth first!");
+        return
+    }
+    let [brace1, brace2] = [0, 0];
+    for (let key in a) {
+        if (a[key] == '(')
+          brace1 ++
+        if (a[key] == ')')
+          brace2 ++
+      }
+    let output = "";
+    if (brace1 === brace2)
+        output = "Even!"
+    else output = "Odd!"
+    document.getElementById("output").innerHTML = output;
 }
