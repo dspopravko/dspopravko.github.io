1219daf3a5999b17d809a42e8084e1ef0f2f80d9
Demid Popravko <demid1498@gmail.com>
2022-05-24 00:31:35 +0400

    Rework counting function

--- src/script.js
+++ src/script.js
@@ -1,24 +1,38 @@
-function checkEven () {
-    let a = document.getElementById('text').value;
-    if (a == 0) {
+function checkParanthesis () {
+    let list = document.getElementById('text').value;
+    let cleanList = [];
+    let x = 0;
+
+    if (list == 0) {
         document.getElementById("output").innerHTML = ("Type smth first!");
         return
+    } else {
+        for (i in list) {
+            if (list[i] == '(' || list[i] == ')') {
+                cleanList[x] = list[i];
+                x++;
+            }
+        }
     }
-    let [brace1, brace2] = [0, 0];
-    for (let key in a) {
-        if (a[key] == '(')
-          brace1 ++
-        if (a[key] == ')')
-          brace2 ++
-      }
-    let output = "";
-    if (brace1 === brace2)
-        output = "All good!"
-    else if (brace1 > brace2) {
-        if (brace1 - brace2 > 1) {
-            output = `You need ${brace1 - brace2} more closed braces`
-        } else output = `You need 1 more closed brace`
-    } else if (brace2 - brace1 > 1) {output = `You need ${brace2 - brace1} more open braces`}
-    else output = `You need 1 more open brace`
+    if (cleanList.length % 2 != 0)
+        output = 'Not even!'
+    else {
+        let bracesCounter = 0;
+        for (let key in cleanList) {
+            if (cleanList[key] == '(') {
+                bracesCounter++;
+            }
+            if (cleanList[key] == ')') {
+                bracesCounter--;
+            }
+            if (bracesCounter < 0) {
+                output = 'Too much closed braces!'
+                break
+            }
+            if (bracesCounter == 0)
+                output = 'Great!'
+            else output = 'Too much open braces!'
+        }
+    }
     document.getElementById("output").innerHTML = output;
 }
